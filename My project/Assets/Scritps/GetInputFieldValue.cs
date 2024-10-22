427cf0a1c437fc9ce93a3c51b397e905a1e62976
ToasterCat47 <Cooper@lyon-den.net>
2024-10-22 13:40:30 -0700

    Adding ImputCard Prefab

---
+++ My project/Assets/Scritps/GetInputFieldValue.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+public class GetInputFieldValue : MonoBehaviour
+{
+    [SerializeField]
+    TMP_InputField field;
+    [SerializeField]
+    TMP_InputField i_flourAmt;
+    [SerializeField]
+    TMP_InputField i_flourCost;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float flourCost = -1;
+            Debug.Log(i_flourAmt.text);
+            Debug.Log(float.TryParse(i_flourCost.text, out flourCost));
+            float flourAmt = -1;
+            Debug.Log(float.TryParse(i_flourAmt.text, out flourAmt));
+            Debug.Log(flourAmt * flourCost);
+        }
+    }
+}
